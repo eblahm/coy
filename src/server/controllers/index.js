0c1bccdf620b5f30b14c01c66eeda006283a4c9d
eblahm <matthew@webs.com>
2015-09-19 12:46:30 -0400

    new homepage and tweak html templates

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 module.exports = fs.readdirSync(__dirname).reduce((memo, fname) => {
   var basename = path.basename(fname, '.js');
   if (basename !== 'index') {
-    memo[basename] = require('./' + basename);
+    memo[basename.replace(/Controller/, '')] = require('./' + basename);
   }
   return memo;
 }, {});
