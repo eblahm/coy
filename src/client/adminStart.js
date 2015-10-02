7b3cc27389fb52e09bd56fcae17eae0cf7cf8cf8
eblahm <matthew@webs.com>
2015-10-02 09:17:42 -0400

    get the epic editor to work within the form

--- src/client/adminStart.js
+++ src/client/adminStart.js
@@ -1,13 +1,14 @@
 
 
-$(() => {
-  new EpicEditor({
-    basePath: '/lib/epiceditor/epiceditor',
-    theme: {
-      editor: '../../../../css/editor-theme.css',
-      preview: '../../../../css/editor-preview-theme.css',
-      base: '../../../../css/editor-base-theme.css'
-    }
-  }).load();
+new EpicEditor({
+  basePath: '/lib/epiceditor/epiceditor',
+  focusOnLoad: true,
+  autogrow: true,
+  textarea: document.getElementById('markdown-content'),
+  theme: {
+    editor: '../../../../css/editor-theme.css',
+    preview: '../../../../css/editor-preview-theme.css',
+    base: '../../../../css/editor-base-theme.css'
+  }
+}).load();
 
-});
