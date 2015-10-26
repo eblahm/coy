ce7e7275032fdbacf2dcc1f627fa1a9e4add95b6
eblahm <matthew@webs.com>
2015-10-25 22:08:35 -0400

    prevent html from gettings saved in meta

--- src/server/controllers/articleController.js
+++ src/server/controllers/articleController.js
@@ -9,6 +9,7 @@ var config = require('config');
 
 const CONTENT_ROOT = config.get('content_root');
 const GITHUB_REPO_ID = config.get('repo');
+const OMITTED_META = ['slug', 'markdown', 'html'];
 
 var isLoggedIn = (req, res, next) => {
   assert.ok(req.session.githubToken, 'must be logged in');
@@ -27,10 +28,14 @@ router.post('', isLoggedIn, (req, res, next) => {
   console.log('attempting to make commit %j', req.body);
 
   contentService.getMeta().then((articleMeta) => {
+    articleMeta = _.reduce(articleMeta, (memo, data, key) => {
+      memo[key] = _.omit(data, OMITTED_META);
+      return memo;
+    }, {});
 
     var nowISOString = new Date().toISOString();
     articleMeta[slug] = _.assign(
-      _.omit(req.body, ['slug', 'markdown', 'html']),
+      _.omit(req.body, OMITTED_META),
       {
         created: _.get(articleMeta, `[${slug}].created`, nowISOString),
         updated: nowISOString
